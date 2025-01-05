25566879fd5acf69637624bc0b8d2449d4b5ba4f
Do Viet Duy <duynghiala@ms365vip.com>
2025-01-05 16:50:57 +0900

    commit service

--- src/main/java/vn/nhacvien/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/nhacvien/laptopshop/LaptopshopApplication.java
@@ -2,12 +2,17 @@ package vn.nhacvien.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
+
+		ApplicationContext nhacvien = SpringApplication.run(LaptopshopApplication.class, args);
+		for (String s : nhacvien.getBeanDefinitionNames()) {
+			System.out.println(s);
+		}
 	}
 
 }
