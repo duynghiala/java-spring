b846a7ae77b19df0467288aa588a9b6dc69b7679
Do Viet Duy <duynghiala@ms365vip.com>
2025-01-09 16:07:05 +0900

    mvc create

---
+++ src/main/java/vn/nhacvien/laptopshop/controller/UserController.java
@@ -0,0 +1,35 @@
+package vn.nhacvien.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.nhacvien.laptopshop.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// @Controller
+// public class UserController {
+
+//     @RequestMapping("/")
+
+//     public String getHomePage() {
+//         return "Hello from Controller";
+//     }
+// }
+
+@RestController
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    public String getHomePage() {
+        return this.userService.handleHello();
+    }
+}
--- src/main/java/vn/nhacvien/laptopshop/services/MyServices.java
+++
@@ -1,8 +0,0 @@
-package vn.nhacvien.laptopshop.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class MyServices {
-
-}
